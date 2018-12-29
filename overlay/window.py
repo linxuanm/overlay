3fceb8c37225de3ded004248333ed37f9bdbc353
David Ma <davidmaamoaix@gmail.com>
2018-12-29 18:47:17 +0800

    Some integrations with tk.Toplevel

--- overlay/window.py
+++ overlay/window.py
@@ -8,19 +8,25 @@ __all__ = [
 
 import tkinter as tk
 
-windows = []
+overlays = []
+
+master = tk.Tk()
+master.withdraw()
 
 class Window:
 
-	def __init__(self, root: tk.Tk = None, **kwargs):
-		'''Initiates an overlay window.
+	def __init__(self, root: tk.Toplevel = None, **kwargs):
+		'''Initiate an overlay window.
 
-		root: tk.Tk, the root of the overlay window.
+		root: tk.Toplevel, the root of the overlay window.
 		size: tuple, the dimension (width, height) of the overlay window.
 		transparent: bool, whether to set the overlay background transparent.
 		transparency: float [0, 1], the transparency of the overlay.
 		'''
 		self._root = root
+
+		'''Create the master (tk.Tk) if not exists.'''
+		global master
 
 		'''Basic configurations.'''
 		self.size = kwargs.get('size', (100, 100))
@@ -40,6 +46,17 @@ class Window:
 		'''Make the overlay float on top of everything.'''
 		self._root.wm_attributes('-topmost', True)
 
+		'''Add self to overlay collections.'''
+		overlays.append(self)
+
+	def hide(self):
+		'''Hide this overlay.'''
+		self._root.withdraw()
+
+	def destroy(self):
+		'''Destroy this overlay.'''
+		self._root.destroy()
+
 	@property
 	def size(self):
 		return self._size
@@ -48,3 +65,7 @@ class Window:
 	def size(self, newSize):
 		self._size = newSize
 		self._root.geometry('%sx%s'%self._size)
+
+	@staticmethod
+	def launch():
+		master.mainloop()
